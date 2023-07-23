bbad7bd36d4deadba19e85f8286df63b64ebb938
Vinayjais <17vinayjaiswal>
2023-07-23 12:26:11 +0530

    updated file with routes

--- Fs_new.js
+++ Fs_new.js
@@ -1,48 +1,9 @@
 const http = require('http');
-const fs = require('fs');
+
 const { log } = require('console');
 const { buffer } = require('stream/consumers');
+ const routes = require ('./routes');
 
-
-const server = http.createServer((req, res) => {
-          const url = req.url;
-          const method = req.method;
-    if( url === '/'){
-        res.write('<html>');
-        res.write('<head><title>Enter message </title></head>');
-        res.write('<dody><form action="/message" method ="POST"><input type="text" name="message"><button type="submit">Send</button> </form><body>');
-        res.write('</html>');
-         return res.end();
-     }
-      if( url ==='/message' && method ==="POST"){
-          const body = [];
-        req.on('data', (chunk) => {
-             console.log(chunk);
-             body.push(chunk);
-        });
-         
-        req.on('end', ()=>{
-
-            const parseBody = Buffer.concat(body).toString();
-            const message = parseBody.split('=')[1];
-            fs.writeFileSync('msg.txt', message);
-            console.log( parseBody);
-
-        });
-
-           
-           res.statusCode = 302;
-           res.setHeader('Location', '/');
-           return res.end();
-
-                      
-      }
-       // res.setHeader('Content-type','text/html');
-        res.write('<html>');
-        res.write('<head><title>My first page </title></head>');
-        res.write('<dody><h1> Hello from my node js server </h1><body>');
-        res.write('</html>');
-        res.end();
-});
+const server = http.createServer( routes);
 
 server.listen(3000);
