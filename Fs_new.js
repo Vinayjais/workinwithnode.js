3031a094857ae139de217e98ecd8b252a21577d1
Vinayjais <17vinayjaiswal>
2023-07-30 09:40:03 +0530

    convert node to exress

--- Fs_new.js
+++ Fs_new.js
@@ -1,9 +1,19 @@
-const http = require('http');
 
-const { log } = require('console');
-const { buffer } = require('stream/consumers');
- const routes = require ('./routes');
 
-const server = http.createServer( routes);
+const exress = require('express');
+//const { log } = require('console');
+//const { buffer } = require('stream/consumers');
+// const routes = require ('./routes');
 
-server.listen(3000);
+const app = exress();
+
+  app.use((req ,res , next) => {
+          console.log('in middleware');
+          next();
+  });
+  app.use((req ,res , next) => {
+    console.log('in another middleware');
+    res.send(`<h1>hello from express !!</h1>`);
+});
+
+app.listen(3000);
